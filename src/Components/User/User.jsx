3abc83092dd5ade3c676440697566d290f9da253
Erich Engler <erich.engler@hotmail.com>
2023-11-26 18:19:40 -0600

    added dotenv file to hide steam api key. running into CORS network error when attempting to fetch data from steam

--- src/Components/User/User.jsx
+++ src/Components/User/User.jsx
@@ -1,7 +1,37 @@
-import './User.css';
+import axios from "axios";
+import "./User.css";
+import { useEffect, useState } from "react";
 
 const User = () => {
-  return <div>User</div>;
-}
+  const [games, setGames] = useState([]);
+
+  useEffect(() => {
+    // ! UNSECURE - ADD .ENV FILE BEFORE PUBLISHING
+    const apiKey = process.env.STEAM_API_KEY;
+    const steamId = "76561197969504268";
+
+    axios
+      .get(
+        `https://api.steampowered.com/IPlayerService/GetOwnedGames/v1/?key=${apiKey}&steamid=${steamId}&format=json`
+      )
+      .then((response) => {
+        setGames(response.data.response.games);
+      })
+      .catch((error) => {
+        console.log("Error fetching Steam user data:", error);
+      });
+  }, []);
+
+  return (
+    <div>
+      <h2>Games Owned:</h2>
+      <ul>
+        {games.map((game) => (
+          <li key={game.appid}>{game.name}</li>
+        ))}
+      </ul>
+    </div>
+  );
+};
 
 export default User;
