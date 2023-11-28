e01a3d35f8f7e12e091ca1618c7ce810edac6b95
Erich Engler <erich.engler@hotmail.com>
2023-11-28 17:11:32 -0600

    styling finished for gamesList page, added filter but not functional yet

--- src/Components/GamesList/GamesList.jsx
+++ src/Components/GamesList/GamesList.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
-import './GamesList.css';
+import "./GamesList.css";
 
 function GamesList() {
   const [games, setGames] = useState([]);
@@ -8,25 +8,42 @@ function GamesList() {
   useEffect(() => {
     const fetchGames = async () => {
       try {
-        const response = await axios.get('/games');
+        const response = await axios.get("/games");
 
         setGames(response.data);
       } catch (error) {
-        console.log('Error fetching games:', error);
+        console.log("Error fetching games:", error);
       }
-    }
+    };
 
     fetchGames();
   }, []);
 
   return (
-    <div>
-      <h2>Games Owned:</h2>
-      <ul>
-        {games.map((game) => (
-          <li key={game.id}>{game.title}</li>
-        ))}
-      </ul>
+    <div className="games-container">
+      <h2 className="games-header">Games</h2>
+      <div className="filter">
+        <span className="filter-by">Name </span> | &nbsp;
+        <span className="filter-by">Hours </span> | &nbsp;
+        <span className="filter-by">Currently Playing</span>
+      </div>
+      {games.length === 0
+        ? "No Games Found"
+        : games.map((game) => (
+            <div key={game.id} className="item-container">
+              <ul className="games-list">
+                <li className="game-title">{game.title}</li>
+                <li>
+                  {game.hours} hours &nbsp;
+                  <button className="notes">View Notes</button>
+                  <br />
+                  {game.playing
+                    ? `✔ Currently Playing`
+                    : `❌ Not Currently Playing`}
+                </li>
+              </ul>
+            </div>
+          ))}
     </div>
   );
 }
