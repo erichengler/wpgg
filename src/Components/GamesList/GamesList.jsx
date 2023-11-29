1e55f57b0504cef40f9fa601e814282b01992100
Erich Engler <erich.engler@hotmail.com>
2023-11-28 18:00:09 -0600

    sort by filter functionality added and working properly

--- src/Components/GamesList/GamesList.jsx
+++ src/Components/GamesList/GamesList.jsx
@@ -4,6 +4,8 @@ import "./GamesList.css";
 
 function GamesList() {
   const [games, setGames] = useState([]);
+  const [sortCriteria, setSortCriteria] = useState('hours');
+  const [sortOrder, setSortOrder] = useState('desc');
 
   useEffect(() => {
     const fetchGames = async () => {
@@ -19,17 +21,58 @@ function GamesList() {
     fetchGames();
   }, []);
 
+  const handleSortBy = (criteria) => () => {
+    if (sortCriteria === criteria) {
+      setSortOrder(sortOrder === 'asc' ? 'desc' : 'asc');
+    
+    } else {
+      setSortCriteria(criteria);
+      setSortOrder('asc');
+    }
+  };
+
+  const sortedGames = games.sort((a, b) => {
+    // Sort based on criteria and order
+    if (sortCriteria === 'name') {
+      return sortOrder === 'desc' 
+        ? b.title.localeCompare(a.title)
+        : a.title.localeCompare(b.title);
+
+    } else if (sortCriteria === 'hours') {
+      return sortOrder === 'asc'
+        ? a.hours - b.hours
+        : b.hours - a.hours;
+
+    } else if (sortCriteria === 'playing') {
+      return sortOrder === 'asc' 
+        ? a.playing - b.playing
+        : b.playing - a.playing
+    }
+    // Default, no sorting
+    return;
+  })
+
   return (
     <div className="games-container">
       <h2 className="games-header">Games</h2>
+
       <div className="filter">
-        <span className="filter-by">Name </span> | &nbsp;
-        <span className="filter-by">Hours </span> | &nbsp;
-        <span className="filter-by">Currently Playing</span>
+        <span className="filter-by" onClick={handleSortBy('name')}>
+          Name {sortCriteria === 'name' && (sortOrder === 'asc' ? '▲' : '▼')}
+        </span> | &nbsp;
+
+        <span className="filter-by" onClick={handleSortBy('hours')}>
+          Hours {sortCriteria === 'hours' && (sortOrder === 'asc' ? '▲' : '▼')}
+        </span> | &nbsp;
+
+        <span className="filter-by" onClick={handleSortBy('playing')}>
+          Currently Playing {sortCriteria === 'playing' && (sortOrder === 'asc' ? '❌' : '✔' )}
+        </span>
       </div>
+      
       {games.length === 0
         ? "No Games Found"
-        : games.map((game) => (
+        : sortedGames.map((game) => (
             <div key={game.id} className="item-container">
               <ul className="games-list">
                 <li className="game-title">{game.title}</li>
@@ -43,7 +86,8 @@ function GamesList() {
                 </li>
               </ul>
             </div>
-          ))}
+        ))
+      }
     </div>
   );
 }
